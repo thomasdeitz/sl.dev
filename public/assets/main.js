db6341a522b68d9dc9ebd3974a1e31fdbff9dba3
Tom Deitz <info@thomasdeitz.com>
2018-02-14 08:34:22 -0700

    reorganization and addition of expanded data items js and styles to come.

--- public/assets/main.js
+++ public/assets/main.js
@@ -8,8 +8,8 @@
   $(document).ready(function () {
 
     if (!Cookies.get('facebookAuth') && !Cookies.get('twitterAuth') && !Cookies.get('linkedinAuth')) {
-      if (window.location.pathname.split("/").slice(-1) != "connect" && window.location.pathname.split("/").slice(-1) != "authenicating" && window.location.pathname.split("/").slice(-1) != "") {
-        window.location.href = "./connect";
+      if (window.location.pathname.split("/").slice(-1) != "preferences" && window.location.pathname.split("/").slice(-1) != "authenicating" && window.location.pathname.split("/").slice(-1) != "") {
+        window.location.href = "./preferences/";
         console.log(window.location.pathname)
       }
     }
